3267c813475b99a3374421a773d77f21790da1b3
kwj5584 <kvvj5584@gmail.com>
2021-02-13 15:52:28 +0900

    JdbcTemplate 작성

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -21,7 +21,7 @@ public class MemberService {
     /** 회원가입 */
     public Long join(Member member){
         // 같은 이름이 있는 중복 회원X
-        validateDuplicateMember(member); // 중복회우너 검증
+        validateDuplicateMember(member); // 중복회원 검증
         memberRepository.save(member);
        return member.getId();
     }
